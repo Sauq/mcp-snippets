f230df003de1534ce2da94956af6af61f7084944
Sauq <69165251+Sauq@users.noreply.github.com>
2021-03-05 18:03:45 +0000

    Update GuiConnecting.java

--- IGN In DiscordSRC/GuiConnecting.java
+++ IGN In DiscordSRC/GuiConnecting.java
@@ -1,1 +1,6 @@
 Client.getInstance().getDiscordRP().update("Multiplayer: " + ip + ((port != 25565) ? (":" + port) : ""), "IGN: " + Minecraft.getMinecraft().getSession().getProfile().getName());
+
+
+
+
+//Add To GuiConnecting
