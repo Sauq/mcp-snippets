0a1ad8c8ab5b53cf62afec71ba5f00a4146fffb8
Sauq <69165251+Sauq@users.noreply.github.com>
2021-03-06 11:26:40 +0000

    Added Custom Keybind
    Save a custom keybind if you changed it.

---
+++ Save Custom Keybind/GameSettings.java
@@ -0,0 +1,5 @@
+//in the Class GameSettings.java go to the method GameSettings make sure that you put addClientKeybinds() before loadOptions()
+
+//In this order
+addClientKeybinds();
+this.loadOptions();
---
+++ Save Custom Keybind/README.md
@@ -0,0 +1,4 @@
+# Save Custom Keybinds
+- Go to GameSettings.java
+- Go to the method GameSettings make sure that you put addClientKeybinds() before loadOptions()
+- Credit is not needed
