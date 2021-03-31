84c0f4ee884334ed20448927672d77eec3015817
Sauq <69165251+Sauq@users.noreply.github.com>
2021-03-31 19:01:15 +0100

    Added Skin Changer

---
+++ Skin Changer Cosmetic/README.md
@@ -0,0 +1,4 @@
+# Skin Changer Cosmetic
+- Create a new class called skinChanger
+- Go to the class RenderPlayer and add it there
+- Credit is not needed
---
+++ Skin Changer Cosmetic/skinChanger.java
@@ -0,0 +1,44 @@
+package clientname.cosmetics.skinchanger;
+
+import clientname.Client;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.AbstractClientPlayer;
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelPlayer;
+import net.minecraft.client.model.ModelSlime;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.entity.RenderPlayer;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraft.util.ResourceLocation;
+
+public class SkinChangerArmy implements LayerRenderer<AbstractClientPlayer>{
+    private final RenderPlayer armyRenderer;
+    private ModelBase armyModel = new ModelPlayer(0, true);
+
+    public SkinChangerArmy(RenderPlayer renderPlayer) {
+        this.armyRenderer = renderPlayer;
+        this.armyModel = renderPlayer.getMainModel();
+    }
+
+    @Override
+    public void doRenderLayer(AbstractClientPlayer entitylivingbaseIn, float p_1771412111, float p_1771413111,
+            float partialTicks, float p_1771415111, float p_1771416111, float p_1771417111, float scale) {
+        if(Client.SkinChangerArmy) {
+            Minecraft.getMinecraft().getTextureManager().bindTexture(new ResourceLocation("armyskin")); //Example with an Army Skin
+            GlStateManager.enableNormalize();
+            GlStateManager.enableBlend();
+            GlStateManager.blendFunc(770, 771);
+            this.armyModel.setModelAttributes(this.armyRenderer.getMainModel());
+            this.armyModel.render(entitylivingbaseIn, p_1771412111, p_1771413111, p_1771415111, p_1771416111, p_1771417111, scale);
+            GlStateManager.disableBlend();
+            GlStateManager.disableNormalize();
+        }
+
+    }
+
+    @Override
+    public boolean shouldCombineTextures() {
+        return true;
+    }
+
+}
