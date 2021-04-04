77a83f19a3bb7a6530d3e003e40edd205418a293
Sauq <69165251+Sauq@users.noreply.github.com>
2021-04-04 09:32:23 +0100

    Added Chroma Chat

---
+++ Chroma Chat/ChromaChat.java
@@ -0,0 +1,14 @@
+/*
+Chroma Chat - By Sauq
+Credit would be Nice but not needed
+*/
+
+//Put this in Client.java
+
+public static int RainbowEffect(int i, float f) {
+		return Color.HSBtoRGB((float)(System.currentTimeMillis() % 20000L) / 1000.0F, 0.8F, 0.8F);
+	}
+
+//And Replace 'int i = this.isEnabled ? this.enabledColor : this.disabledColor;' with:
+
+int i = this.isEnabled ? Cleint.RainbowEffect : this.disabledColor;
---
+++ Chroma Chat/README.md
@@ -0,0 +1,3 @@
+# Simple Chroma Chat!
+- Credit is not needed
+- YT Link: https://www.youtube.com/watch?v=TepPQx2fTkg&lc=UgxpaaQf3Xu5Ezo83cR4AaABAg
