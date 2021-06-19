be6eba35844a8a4c7fed23145418cb7e69fa3f00
Sauq <69165251+Sauq@users.noreply.github.com>
2021-06-19 01:12:27 +0100

    Delete GuiScreen.java

--- GuiScreen.java
+++
@@ -1,7 +0,0 @@
-public void sendChatMessage(String msg) {
-  if(msg.equalsIgnoreCase(".Hi")) {
-    Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(new ChatComponentText("Hello " + Minecraft.getMinecraft().getSession().getUsername();))
-  }else {
-    this.sendChatMessage(msg, true);
-  }
-}
